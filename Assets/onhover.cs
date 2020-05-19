600b669912dff5b162c3c650b18a714440469a5c
Martin <martin.lai@edu.sait.ca>
2020-05-19 04:21:47 -0600

    hello

--- Assets/onhover.cs
+++ Assets/onhover.cs
@@ -17,17 +17,3 @@ public class onhover : MonoBehaviour
     }
 }
 
-public class OnMouseOver : MonoBehaviour
-{
-    void OnMouseOver()
-    {
-        //If your mouse hovers over the GameObject with the script attached, output this message
-        
-    }
-
-    void OnMouseExit()
-    {
-        //The mouse is no longer hovering over the GameObject so output this message each frame
-        Debug.Log("Mouse is no longer on GameObject.");
-    }
-}
